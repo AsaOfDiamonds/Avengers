e9cac3a57300c8cf02918f432383951228844713
1@$@1github <asaofdiamonds@gmail.com>
2018-12-03 14:16:46 -0700

    added dynamic routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import AvengersList from "./components/AvengersList";
+import AvengerPage from "./components/AvengerPage";
 import avengers from "./data";
 import './App.css';
 
@@ -16,7 +17,8 @@ class App extends Component {
     return (
       <div className="App">
         <Route exact path='/' component={Home} />
-        <Route path='/avengers' component={AvengersList} />
+        <Route exact path='/avengers' component={AvengersList} />
+        <Route path='/avengers/:id' component={AvengerPage} />
       </div>
     );
   }
