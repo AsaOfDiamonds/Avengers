abeac28aca8dbb4810f2646328d80faaa040cf05
1@$@1github <asaofdiamonds@gmail.com>
2018-12-04 11:05:18 -0700

    did first follow along for today, but it broke at end

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,11 +1,10 @@
 import React from "react";
-import avengers from "../data";
 import { Link } from "react-router-dom";
 
 function AvengersList() {
     return (
         <div className='characters-list-wrapper'>
-            {avengers.map(avenger => (
+            {props.avengers.map(avenger => (
                 <div className='character-card' key={avenger.id}>
                     <img src={avenger.thumbnail} alt={avenger.name} />
                     <h2><Link to={`/avengers/${avenger.id}`}>{avenger.name}</Link></h2>
